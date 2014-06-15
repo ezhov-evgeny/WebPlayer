763a7132cf9f076dc866f7e22dc7ca1d8c1c3861
Evgeny Ezhov <ezhov.evgeny@gmail.com>
2014-06-15 21:38:26 +0400

    prepare for JS coding

--- iTunesWeb/js/player.js
+++ iTunesWeb/js/player.js
@@ -1,3 +1,19 @@
 /**
  * Created by Evgeny Ezhov on 15.05.2014.
  */
+var trackListTable = document.getElementById('track_list');
+var trackRows = trackListTable.getElementsByTagName('tr');
+
+function changeStyle(item) {
+    var selectedStyleIdentity = 'selectedStyle';
+//    if (item.className.indexOf(selectedStyleIdentity) > 0) {
+//        return;
+//    }
+    $('.' + selectedStyleIdentity).removeClass(selectedStyleIdentity)
+    item.addClass(selectedStyleIdentity);
+
+    if (item == 'undefined' || item.id == 'allStyles') {
+        trackRows.forEach(function(element) {element.display = true;});
+    }
+
+}
